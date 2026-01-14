21e2de74cc61f3a7eed6f67ad714544eb144acbb
Salmanrehman99 <salmansyedian@gmail.com>
2026-01-14 21:22:09 +0100

    Add files via upload

---
+++ Infrastructure/s3.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_s3_bucket" "avatars" {
+  bucket = "grocerymate-avatars-unique-16623"  # Replace with a unique name
+
+  tags = {
+    Name        = "grocerymate-avatars"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "avatars_acl" {
+  bucket = aws_s3_bucket.avatars.bucket
+  acl    = "private"  # Use acl here in a separate resource
+}
+
+resource "aws_s3_bucket_versioning" "avatars_versioning" {
+  bucket = aws_s3_bucket.avatars.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
