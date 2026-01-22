d7b17884a486289bc7bdbaf60be1975b7cf8b8c7
Salmanrehman99 <salmansyedian@gmail.com>
2026-01-22 23:56:08 +0100

    Update s3.tf

--- Infrastructure/s3.tf
+++ Infrastructure/s3.tf
@@ -2,10 +2,10 @@
 # S3 Bucket
 ########################
 resource "aws_s3_bucket" "avatars_bucket" {
-  bucket = "grocerymate-avatars-unique-16623"
+  bucket = var.bucket_name  
 
   tags = {
-    Name        = "grocerymate-avatars"
+    Name        = "grocerymate-avatars"  
     Environment = "Dev"
   }
 }
@@ -19,65 +19,4 @@ resource "aws_s3_bucket_versioning" "avatars_bucket_versioning" {
   versioning_configuration {
     status = "Enabled"
   }
-}
-
-########################
-# IAM Policy (S3 Full Access – Bucket Scoped)
-########################
-resource "aws_iam_policy" "avatars_s3_policy" {
-  name        = "grocerymate-avatars-s3-full-access"
-  description = "Full access to grocerymate avatars S3 bucket"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = ["s3:*"]
-        Resource = [
-          aws_s3_bucket.avatars_bucket.arn,
-          "${aws_s3_bucket.avatars_bucket.arn}/*"
-        ]
-      }
-    ]
-  })
-}
-
-########################
-# IAM Role (for EC2 + SSM)
-########################
-resource "aws_iam_role" "avatars_role" {
-  name = "grocerymate-avatars-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = [
-            "ec2.amazonaws.com",
-            "ssm.amazonaws.com"
-          ]
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-########################
-# Attach Policy to Role
-########################
-resource "aws_iam_role_policy_attachment" "avatars_attach" {
-  role       = aws_iam_role.avatars_role.name
-  policy_arn = aws_iam_policy.avatars_s3_policy.arn
-}
-
-########################
-# IAM Instance Profile (for EC2)
-########################
-resource "aws_iam_instance_profile" "avatars_profile" {
-  name = "grocerymate-avatars-profile"
-  role = aws_iam_role.avatars_role.name
 }
