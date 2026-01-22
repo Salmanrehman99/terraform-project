06e96713c7a0fcd5863765e0f340e46be41dd823
Salmanrehman99 <salmansyedian@gmail.com>
2026-01-22 22:52:10 +0100

    Remove unused input variable definitions
    Removed input variable definitions for database password, S3 bucket name, EC2 key pair name, and public key path.

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -15,31 +15,6 @@ provider "aws" {
 
 }
 
-# -----------------------
-# Input Variables
-# -----------------------
-variable "db_password" {
-  description = "Password for the PostgreSQL RDS instance"
-  type        = string
-  sensitive   = true
-}
-
-variable "bucket_name" {
-  description = "Name of your existing S3 bucket"
-  type        = string
-}
-
-variable "key_pair_name" {
-  description = "Name of the AWS EC2 Key Pair"
-  type        = string
-  default     = "grocerymate-key"
-}
-
-variable "public_key_path" {
-  description = "Path to your EC2 public key file"
-  type        = string
-  default     = "C:\\Users\\HP\\.ssh\\grocerymate-key.pub"
-}
 
 # -----------------------
 # Fetch Latest Amazon Linux 2 AMI
