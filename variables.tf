29be9317545c2ede9a3f71bf3bf3f1d6ada589c8
Salmanrehman99 <salmansyedian@gmail.com>
2026-01-22 22:47:42 +0100

    Refine variable descriptions in variables.tf
    Updated descriptions for input variables in variables.tf.

--- variables.tf
+++ variables.tf
@@ -1,5 +1,26 @@
+# -----------------------
+# Input Variables
+# -----------------------
+
 variable "db_password" {
-  description = "PostgreSQL RDS password"
+  description = "Password for the PostgreSQL RDS instance"
   type        = string
   sensitive   = true
+}
+
+variable "bucket_name" {
+  description = "Name of your existing S3 bucket"
+  type        = string
+}
+
+variable "key_pair_name" {
+  description = "Name of the AWS EC2 Key Pair"
+  type        = string
+  default     = "grocerymate-key"
+}
+
+variable "public_key_path" {
+  description = "Path to your EC2 public key file"
+  type        = string
+  default     = "C:\\Users\\HP\\.ssh\\grocerymate-key.pub"
 }
